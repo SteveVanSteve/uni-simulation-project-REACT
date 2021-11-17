95a16bf7dadd44b72f09b88a289ed933c4bc95ad
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-17 20:56:52 +0000

    Basic landing page/root - needs to be 2 columns

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css'; 
 
 function App() {
+  const title = 'Welcome to Grid-Scout';
+
   return(
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>A grid simulation web service</p>
       </div>
     </div>
   );
