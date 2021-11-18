f7bf22fb2b81d4dcc032fd1976e94b471e1acbf5
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-18 21:29:50 +0000

    generic chart data is in - now to make 2 columns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import './App.css'; 
+import React, { Component } from 'react';
+import './App.css';
+import Chart from './components/Chart';
 
-function App() {
-  const title = 'Welcome to Grid-Scout';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
 
-  return(
-    <div className="App">
-      <div className="content">
-        <h1>{ title }</h1>
-        <p>A grid simulation web service</p>
+  componentWillMount(){
+   // this.getchartData(); // this should be this.getChartData();
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,56 +1,71 @@
 import React, {Component} from 'react';
-import {Bar, Line} from 'react-chartjs-2';
+import {Bar, Line, Pie} from 'react-chartjs-2';
 
 class Chart extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            chartData:props.chartData
-        }
-    }    
-
-    static defaultProps = {
-        displayTitle: true,
-        displayLegend: true,
-        legendPosition:'bottom'
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
     }
+  }
 
-    render(){
-            return (
-            <div className="chart">
-            <Bar
-                data={this.state.chartData}
-                options={{
-                    title:{
-                        display:this.props.displayTitle,
-                        text:'Simulation Results of charging scenarios (TEST)',
-                        fontSize:25
-                    },
-                    legend:{
-                        display:this.props.displayLegend,
-                        position:this.props.legendPosition
-                    }
-                }}
-            />
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'City'
+  }
 
-        <Line
-    	    data={this.state.chartData}
-	        options={{
-                title:{
-                    display:this.props.displayTitle,
-                    text:'Simulation Results of charging scenarios (TEST)',
-                    fontSize:25
-                },
-                legend:{
-                    display:this.props.displayLegend,
-                    position:this.props.legendPosition
-                }
-            }}
+  render(){
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
         />
 
-        </div>
-        )
-    }
+        <Line
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 export default Chart;
