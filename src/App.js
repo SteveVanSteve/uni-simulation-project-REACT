77f8f20d1f2a2c54eabb5e2574f2b5b31b50e596
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-19 15:42:18 +0000

    more updates

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
 import logo from './Grid_Scout_Logo.png.png';
-import Chart from './components/Chart';
+import './App.css';
 
-
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      chartData:{}
-    }
-  }
-
-  componentWillMount(){
-    this.getChartData();
-  }
-
-  getChartData(){
-    // Ajax calls here
-    this.setState({
-      chartData:{
-        label: "",
-        labels: [0.00, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00, 8.00, 9.00, 10.00, 11.00,
-          12.00, 13.00, 14.00, 15.00, 16.00, 17.00, 18.00, 19.00, 20.00, 21.00, 22.00, 23.00],
-        datasets:[
-          {
-            label:'Total Power onto phase',
-            data:[  
-              ],
-            backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
-            ]
-          }
-        ]
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Grid-Scout!</h2>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <Chart chartData={this.state.chartData} legendPosition="bottom"/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Grid-Scout!
+        </p>
+        </header>
+    </div>
+  );
 }
-
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,42 +1,42 @@
-import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+// import React, {Component} from 'react';
+// import {Line} from 'react-chartjs-2';
 
-class Chart extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      chartData:props.chartData
-    }
-  }
+// class Chart extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       chartData:props.chartData
+//     }
+//   }
 
-  static defaultProps = {
-    displayTitle:true,
-    displayLegend: true,
-    legendPosition:'right'
-  }
+//   static defaultProps = {
+//     displayTitle:true,
+//     displayLegend: true,
+//     legendPosition:'right'
+//   }
 
-  render(){
-    return (
-      <div className="chart">
+//   render(){
+//     return (
+//       <div className="chart">
         
 
-        <Line
-          data={this.state.chartData}
-          options={{
-            title:{
-              display:this.props.displayTitle,
-              fontSize:25
-            },
-            legend:{
-              display:this.props.displayLegend,
-              position:this.props.legendPosition
-            }
-          }}
-        />
+//         <Line
+//           data={this.state.chartData}
+//           options={{
+//             title:{
+//               display:this.props.displayTitle,
+//               fontSize:25
+//             },
+//             legend:{
+//               display:this.props.displayLegend,
+//               position:this.props.legendPosition
+//             }
+//           }}
+//         />
 
-      </div>
-    )
-  }
-}
+//       </div>
+//     )
+//   }
+// }
 
-export default Chart;
+// export default Chart;
