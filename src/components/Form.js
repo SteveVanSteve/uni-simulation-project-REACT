bc159135136e6deee101e2acdee21479b5846089
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-26 15:51:30 +0000

    index.html is cleared out and js being fixed pt 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,53 +1,63 @@
 import React from 'react';
 
 class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('The selected BackgroundSet: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
+  constructor(props) {
+    super(props);
+    this.state = {value: []};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('The backgroundSet selected is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Select the Background Setting:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="No Electric Heating">No Electric Heating</option>
+            <option value="Additional Electric Heating">Additional Electric Heating</option>
+            <option value="Primary Electric Heating">Primary Electric Heating</option>
+          </select>
+        </label>
+        <input type="submit" value="Select" />
+        <div>
+        <p></p>
+        <label>Enter the number of Electric Cars:
+          <input type="text" />
+        </label>
+        <input type="submit" value="Enter"/>
+        </div>
+          <div>
+          <p></p>
           <label>
-            Select the BackgroundSet:
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="No Electric Heating">No Electric Heating</option>
-              <option value="Additional Electric Heating">Additional Electric Heating</option>
-              <option value="Primary Electric Heating">Primary Electric Heating</option>
-            </select>
+            Add this Configuration
           </label>
-          <input type="submit" value="Select" />
-          <div>
-          <p></p>
-          {/* Text box for number of Elecrtic Vehicles */}
-          <label>Enter the number of Electric Vehicles:
-            <input type="text" />
-          </label>
-          <input type="submit" value="Enter"/>
+          <input type="submit" value="Add"/>
           </div>
-          <div>
           <p></p>
-              {/* 'Run Simulation Configuration' button */}
-              <label>Run Simulation Configuration</label>
+          <div>
+             <label>Run Simulation Configuration</label>
               <input value="Run Simulation" type="submit" />
           </div>
-        </form>
-      );
-    }
+          <p></p>
+          <div>
+             <label>Get Simulation Results</label>
+             <input type="submit" value="Get"/>
+          </div>
+      </form>
+    );
   }
+}
 
 export default Form;
 
