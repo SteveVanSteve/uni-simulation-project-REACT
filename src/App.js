c8804dae4f22e67282ab47519955deb090feff49
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-27 20:16:57 +0000

    Working on App / getConfig pt2

--- src/App.js
+++ src/App.js
@@ -4,28 +4,38 @@ import './App.css';
 import Form from './components/Form';
 import SimulationChart from './components/SimulationChart';
 
+function App() {
+
+  const [loading, setLoading] =useState(false)
+  const[simulationConfig, setSimulationConfig] = useState()
+  const [simulationResult, setSimulationResult] = useState()
+
+
+  useEffect(() => {
+    if (!simulationConfig){
+    getSimulationConfig()}
+	});
 
 function App() {
-   return (
-     <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Welcome to Grid-Scout!
-         </p>
-         </header>
-         <p>
-           <ResultsAPI />
-         </p>
-           <ConfigAPI />
-         <p>
-           <Form />
-         </p>
-         <p>
-         <SimulationChart />
-         </p>
-     </div>
-   );
- }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Grid-Scout!
+        </p>
+        </header>
+        <p>
+          <Form getSimulationConfig={getSimulationConfig} getSimulationResult={getSimulationResult} />
+        </p>
+        {JSON.stringify(simulationConfig, null, 2)}
+        <p>
+        <SimulationChart simulationResult={simulationResult} />
+        </p>
+    </div>
+  );
+}
 
 export default App;
