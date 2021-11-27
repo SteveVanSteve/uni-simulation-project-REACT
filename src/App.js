71d6ae6787caecd666c84d2226cf299847782807
Steve Shields <stevie.shields@hotmail.co.uk>
2021-11-27 20:28:12 +0000

    Fixing App.js -> syntax error

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SimulationChart from './components/SimulationChart';
 
 function App() {
 
+
   const [loading, setLoading] =useState(false)
   const[simulationConfig, setSimulationConfig] = useState()
   const [simulationResult, setSimulationResult] = useState()
@@ -16,7 +17,41 @@ function App() {
     getSimulationConfig()}
 	});
 
-function App() {
+
+  const getSimulationConfig = () => {
+		setLoading(true);
+		const apiUrl = `http://127.0.0.1:8000/simulationconfig`;
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((simulationConfig) => {
+        setLoading(false)
+        setSimulationConfig(simulationConfig)
+			});
+  }
+
+  const getSimulationResult =() =>{
+    setLoading(true);
+    const requestOptions ={
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(simulationConfig.results)
+    };
+
+		const apiUrl = `http://127.0.0.1:8000/simulationresult/`;
+		fetch(apiUrl, requestOptions)
+			.then((data) => data.json())
+			.then((simulationResult) => {
+        setLoading(false)
+        console.log("result")
+        console.log(simulationResult)
+        setSimulationResult(simulationResult)
+			})
+      .catch((error) => {
+        console.log(error)
+      });;
+  }
+
+  }
 
 
   return (
